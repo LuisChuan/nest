ad95d69ca2f0c7e30efc101b81dcab6a14007f95
liuzhi <727751846@qq.com>
2023-01-29 09:32:27 +0800

    feat:curd

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,9 +1,9 @@
-import {IsNotEmpty,IsString, Length} from 'class-validator'
+import { IsNotEmpty, IsString, Length } from 'class-validator'
 export class CreateUserDto {
-  @IsNotEmpty()//验证是否为空
+  @IsNotEmpty() //验证是否为空
   @IsString() //是否为字符串
-  @Length(5,10,{
-    message:"不能超过十个字符"
+  @Length(5, 10, {
+    message: '不能超过十个字符'
   })
-  name:string
+  name: string
 }
